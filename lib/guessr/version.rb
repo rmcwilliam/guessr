b56590b66ea84fac8e6f9f8c42db897acea43b0f
Brit Butler <redline6561@gmail.com>
2015-10-07 13:27:42 -0400

    Initial commit.

---
+++ lib/guessr/version.rb
@@ -0,0 +1,3 @@
+module Guessr
+  VERSION = "0.1.0"
+end
---
+++ test/guessr_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class GuessrTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Guessr::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
